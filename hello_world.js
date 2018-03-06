3f8dbfdc77e8c8faefaf7dc8140d6811f54a6432
hxm <724114487@qq.com>
2018-03-06 16:35:56 +0800

    node学习的第一次提交

---
+++ hello_world.js
@@ -0,0 +1,5 @@
+/**
+ * Created by Administrator on 2018/3/2 0002.
+ */
+
+console.log("hello world!!");
---
+++ http_sever_ex_1.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Administrator on 2018/3/2 0002.
+ */
+var http = require("http");
+var server = http.createServer(function(request,response){
+    response.end("zhufengpeixun.cm");
+})
+server.listen(8080);
